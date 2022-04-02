5081502c61938146992a0e98660812c4059c5480
Mat7400 <mmatuha74@gmail.com>
2022-04-02 12:53:16 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # unity2d_2
  unity 2d based on free platform game asset
+pack https://assetstore.unity.com/packages/2d/environments/free-platform-game-assets-85838
---
+++ script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // допустим хотим двигать объект "image 3" по нажатию клавиш WASD
+        // по нажатию кнопки прилетает оповещение что кнопка нажата в Винду (ОС)
+        // у кнопки есть код допустим что клавиша А это код 044
+        // приложениям надо отслеживать эти оповещения и понимать какая клавиша нажата
+        // 
+        // На сенсорном экране телефона есть координаты и есть координаты нажатия на экран
+        // можно например сделать область управления (круг) и если координаты нажатия
+        // попали в сектор круга "влево" то и объект едет влево
+        // 
+        // можно сприпт управления потом прикрутить к нашей старой игре
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var gobject = GameObject.Find("image 3");
+        var transf = gobject.GetComponent<Transform>();
+        int speed = 1;
+        //
+        if (Input.GetKeyDown( KeyCode.W ) )
+        {
+            //w up
+            transf.Translate(0, speed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //s down
+            transf.Translate(0, -speed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //A left
+            transf.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //D right
+            transf.Translate(speed, 0, 0);
+        }
+    }
+}
